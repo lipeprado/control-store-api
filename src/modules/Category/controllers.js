b6a86455b17ad73479442b51f0c649518b606e6a
Filipe Prado <fi-lipe2010@hotmail.com>
2019-01-04 11:51:57 -0200

    CRUD Categories

--- src/modules/Category/controllers.js
+++ src/modules/Category/controllers.js
@@ -19,6 +19,66 @@ export const createCategory = async (req, res, next) => {
 
   return next();
 };
-export const updateCategory = async (req, res, next) => {};
-export const deleteCategory = async (req, res, next) => {};
-export const getAllCategories = async (req, res, next) => {};
+
+export const updateCategory = async (req, res, next) => {
+  const currentUser = req.user;
+  const { id } = req.params;
+
+  const { body } = req;
+  try {
+    const updatedCategory = await CategoryModel.findOneAndUpdate(
+      {
+        creatorId: currentUser.id,
+        _id: id,
+      },
+      body,
+      { new: true },
+    );
+    if (!updatedCategory) {
+      return res
+        .status(HTTPStatus.NOT_FOUND)
+        .json({ message: 'Category not Found' });
+    }
+
+    return res.status(HTTPStatus.OK).json(updatedCategory);
+  } catch (e) {
+    e.status = HTTPStatus.BAD_REQUEST;
+    return next(e);
+  }
+};
+
+export const deleteCategory = async (req, res, next) => {
+  const currentUser = req.user;
+  const { id } = req.params;
+  try {
+    const deletedCategory = await CategoryModel.findOneAndDelete({
+      creatorId: currentUser.id,
+      _id: id,
+    });
+    if (!deletedCategory) {
+      return res
+        .status(HTTPStatus.NOT_FOUND)
+        .json({ message: 'Category not Found' });
+    }
+    return res
+      .status(HTTPStatus.OK)
+      .json({ message: 'Category deleted with success.' });
+  } catch (e) {
+    e.status = HTTPStatus.BAD_REQUEST;
+    return next(e);
+  }
+};
+
+export const getAllCategories = async (req, res, next) => {
+  const currentUser = req.user;
+
+  const query = { creatorId: currentUser.id };
+
+  try {
+    const categories = await CategoryModel.find(query).sort({ title: 1 });
+    return res.status(HTTPStatus.OK).json({ categories });
+  } catch (error) {
+    res.status(HTTPStatus.BAD_REQUEST).json({ error });
+  }
+  return next();
+};
